adbb2640ce47fc3ca5ecd248a95532b46fa97ffb
098 Mathesh Raju <matheshraju670@gmail.com>
2024-05-14 21:27:06 +0530

    added href to table of content

--- app/blog/[...path]/page.tsx
+++ app/blog/[...path]/page.tsx
@@ -2,24 +2,53 @@
 import { fetchpost } from "@/app/api/db";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useEffect, useState } from "react";
+import { parse } from "node-html-parser";
 
 export default function Blog({ params }: any) {
-  const [post, setPost] = useState<any>();
+  const [post, setPost] = useState<ContentProps>();
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetchpost(params.path);
+      if (res) {
+        // Modify actualcontent to include href links for table of content
+        let modifiedContent = parse(res.actualcontent);
+        res.tableofContent.forEach((toc) => {
+          const id = toc.toLowerCase().replace(/\s+/g, "-");
+
+          const headingElement = modifiedContent.querySelector(
+            `h1:contains("${toc}")`
+          );
+          if (headingElement) {
+            const anchor = `<a id="${id}" class="text-black font-bold text-2xl">${toc}</a>`;
+
+            headingElement.replaceWith(anchor);
+          }
+        });
+        res.actualcontent = modifiedContent.toString();
+      }
       setPost(res);
     };
     fetchData();
-  }, []);
+  }, [params.path]);
+
+  useEffect(() => {
+    if (post) {
+      const actualcontent = post.actualcontent.toLowerCase();
+      const tableofContent = post.tableofContent;
+      console.log(actualcontent.match(tableofContent[0].toLowerCase()));
+      console.log(
+        actualcontent.toLowerCase().includes(tableofContent[0].toLowerCase())
+      ); // Will now log the correct value
+    }
+  }, [post]);
 
   return (
     <>
       {post ? (
         <>
           <div className="sm:m-10 grid grid-cols-1 sm:grid-cols-5 gap-4">
-            <div className="bg-gray-100 px-4 py-3 text-gray-800 break-words max-w-md rounded sm:col-span-1">
+            <div className="bg-gray-50 px-4 py-3 text-gray-800 break-words max-w-md rounded sm:col-span-1">
               <div className="mx-auto text-xl font-semibold">
                 <strong>Table of Content</strong>
               </div>
@@ -28,7 +57,7 @@ export default function Blog({ params }: any) {
                   <li key={index}>
                     <a
                       className="block hover:bg-gray-200 px-2 py-1 rounded"
-                      href={`#${content}`}
+                      href={`#${content.toLowerCase().replace(/\s+/g, "-")}`}
                     >
                       {content}
                     </a>
@@ -37,9 +66,9 @@ export default function Blog({ params }: any) {
               </ul>
             </div>
             <div className="sm:col-span-4">
-              <Card className="justify-center p-2 m-1 sm:mr-20">
+              <Card className="justify-center p-2 m-1 bg-blue-50 sm:mr-20">
                 <CardHeader>
-                  <CardTitle className="text-center font-roboto font-bold text-2xl">
+                  <CardTitle className="text-center font-roboto font-bold text-4xl">
                     {post.title}
                   </CardTitle>
                 </CardHeader>
@@ -48,7 +77,7 @@ export default function Blog({ params }: any) {
                   alt="image description"
                   className="w-full h-auto max-w-lg mx-auto my-4 rounded-xl"
                 />
-                <CardContent className="text-center font-roboto text-lg">
+                <CardContent className="text-center font-roboto text-md ">
                   <div
                     dangerouslySetInnerHTML={{ __html: post.actualcontent }}
                   />
@@ -62,4 +91,15 @@ export default function Blog({ params }: any) {
       )}
     </>
   );
+}
+
+interface ContentProps {
+  id: number;
+  title: string;
+  description: string;
+  actualcontent: string;
+  imageurl: string;
+  date: string;
+  likes: number;
+  tableofContent: string[];
 }
